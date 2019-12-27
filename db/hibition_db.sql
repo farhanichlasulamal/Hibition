7b352510626e5474d1f9634235c57211334f4e96
farhanichlasulamal <farhan.ichlasulamal@student.uns.ac.id>
2019-12-27 14:44:05 +0700

    Update hibition_db.sql

--- db/hibition_db.sql
+++ db/hibition_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2019 at 07:07 PM
+-- Generation Time: Dec 27, 2019 at 08:43 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -91,7 +91,15 @@ CREATE TABLE `artikel` (
 
 INSERT INTO `artikel` (`id_artikel`, `judul`, `ringkasan_artikel`, `tanggal_artikel`, `waktu_artikel`, `gambar_artikel`, `id_penulis`, `id_kategori_artikel`) VALUES
 (1, '4 Cara Terbaik Membuat Kopi Tubruk yang Benar', 'Kopi tubruk telah menjadi ciri khas budaya kopi di Indonesia. Istilah tubruk berasal dari Bahasa Jawa yang berarti tabrak atau berbenturan. Untuk membuat kopi tubruk yang benar ada cara-cara yang harus lo perhatikan!', '2019-12-02', '10:25:14', 'Kopi_1.jpeg', 'M0516036', 1),
-(2, 'Taste a Better Life in 5 Ways', 'Membuat hidup lo lebih baik itu bisa lo lakukan dengan mudah dan tidak mesti harus dengan perubahan yang besar-besaran. Berikut ini 5 langkah yang bisa buat hidup lo lebih baik!', '2019-12-02', '11:11:12', 'relax_1.jpg', 'M0516036', 1);
+(2, 'Taste a Better Life in 5 Ways', 'Membuat hidup lo lebih baik itu bisa lo lakukan dengan mudah dan tidak mesti harus dengan perubahan yang besar-besaran. Berikut ini 5 langkah yang bisa buat hidup lo lebih baik!', '2019-12-02', '11:11:12', 'relax_1.jpg', 'M0516036', 1),
+(3, '5 Inspirasi Gadget yang Mesti Lo Punya', 'Hidup udah sulit, jangan ditambah sulit lagi, Bro! Nah, sekarang udah banyak teknologi bisa membuat hidup lo lebih mudah. Ini dia beberapa inspirasi gadget yang bisa memudahkan lo dalam beraktivitas.', '2019-12-04', '05:06:12', 'gadget_1.jpg', 'M0516036', 1),
+(4, 'Rawat Gitar Lo dengan Tips dari Nikita Dompas!', 'Jangan sampai gitar kesayangan lo performa-nya berkurang karena perawatan yang kurang baik, Urbaners! Seperti Nikita Dompas, ia selalu melakukan hal-hal kecil seperti ini untuk mempertahankan kualitas suara gitar.', '2019-12-05', '21:17:25', 'guitar_1.jpg', 'M0516036', 2),
+(5, 'Tips Memilih dan Merawat Kaos Kaki dengan Tepat', 'Jangan anggap remeh kaos kaki! Berikut padu padan nan kece dengan kaos kaki.', '2019-12-06', '10:23:06', 'sock_1.jpg', 'M0516036', 1),
+(6, '5 Tips Improve Your Creative Thinking', 'Ada banyak cara buat menambah skill kreatif lo. Selain berguna untuk melihat sesuatu dari kacamata dan perspektif yang berbeda, berpikir kreatif untuk menangkap kesempatan-kesempatan yang tidak biasa untuk mendapatkan keuntungan.', '2019-12-07', '10:25:14', 'think_1.jpg', 'M0516036', 1),
+(7, 'Outdoor Activity: Apparel Tips From Visval Bags', 'Pastikan kalau perlengkapan outdoor activity lo mendukung ya, Urbaners! Jangan karena kurang informasi, bukannya membantu malah jadi merepotkan. Apalagi kalau lo akan melakukan aktivitas di cuaca ekstrem. Berikut ini tips dari Visval Bags agar lo bisa menemukan apparel yang tepat untuk aktivitas outdoor.', '2019-12-08', '07:20:21', 'outdoor_activity_1.jpg', 'M0516036', 1),
+(8, 'Cara Mudah Memotret Mainan Favorit Lo!', 'Lo seorang kolektor action figure? Pastinya hobi banget mengabadikan koleksi lo tersebut. Nah, kalau gitu lo harus tau bagaimana teknik dasar toy photography. Hal ini bukan hal yang sulit kok, Urbaners. Anggota komunitas Behind The Scene Toys Photography (@BTSTP_ID) yakni Gibran Risnutama dan Mohammad Adji sharing tentang basic step kalau lo mau memotret mainan, Urbaners. Check this out!', '2019-12-09', '11:11:12', 'toys_photography.jpg', 'M0516036', 3),
+(9, 'Top 5 Playlist Poppie Efek Rumah Kaca', 'Buat lo yang lagi mencari playlist terbaru dalam bermusik, nggak harus lagu-lagu baru, terkadang nostalgia dengan lagu-lagu lama juga akan membuat lo fresh. Nah, lo bisa nyontek playlist seru dari Poppie personel Efek Rumah Kaca ini. Musik dari berbagai dekade ada disini!', '2019-12-10', '21:15:12', 'efek_rumah_kaca_1.jpg', 'M0516036', 2),
+(10, 'Memilih Sepatu Hiking Sekaligus Buat Gaya', 'Perlengkapan terpenting hiking adalah keamanan dan kualitasnya, tetapi sekarang tidak hanya itu lho, Urbaners! Banyak sepatu hiking dengan model yang fashionable. Nah, untuk mendukung itu semua, ada beberapa hal yang harus lo perhatikan terlebih dahulu nih, Urbaners!', '2019-12-11', '23:11:11', 'hiking_1.jpg', 'M0516036', 4);
 
 -- --------------------------------------------------------
 
@@ -114,21 +122,6 @@ INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `gambar_kategori`) VALUE
 (2, 'Music', 'music.jpg'),
 (3, 'Photography', 'photography.jpg'),
 (4, 'Sneaker', 'Sneaker.jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pesan`
---
-
-CREATE TABLE `pesan` (
-  `id_pesan` int(10) NOT NULL,
-  `judul_pesan` varchar(100) NOT NULL,
-  `tanggal_pesan` datetime NOT NULL,
-  `isi_pesan` text NOT NULL,
-  `nama_pengirim` varchar(50) NOT NULL,
-  `email_pengirim` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -158,7 +151,9 @@ INSERT INTO `step` (`id_step`, `nomor_urut`, `judul_step`, `isi_step`, `gambar_s
 (6, 2, 'Don\'t waste your time', 'Waktu nggak bisa terulang lagi, jangan dihabiskan buat yang nggak penting.', 'relax_3.jpg', 2),
 (7, 3, 'Keep trying', 'Kalau hidup lagi \'Di Bawah\', terus berusaha dan jangan menyerah!', 'relax_4.jpg', 2),
 (8, 4, 'Open minded', 'Punya wawasan luas bisa bantu menghadapi tekanan dan masalah.', 'relax_5.jpeg', 2),
-(9, 5, 'Seek for balance', 'Kembalikan semangat dengan \'Berhenti\' dan rehat sejenak.', 'relax_6.jpg', 2);
+(9, 5, 'Seek for balance', 'Kembalikan semangat dengan \'Berhenti\' dan rehat sejenak.', 'relax_6.jpg', 2),
+(10, 1, 'Wind & Water Resistant', 'Pilih jaket berbahan polyester atau gore-tex agar terlindung dari berbagai cuaca.', 'outdoor_activity_2.jpg', 7),
+(11, 1, 'Bahan dan Model', 'Agar bisa digunakan untuk gaya lo sehari-hari, pilihlah sepatu hiking dengan bahan dan model yang sesuai dengan karakter lo, Urbaners!', 'hiking_2.jpg', 10);
 
 -- --------------------------------------------------------
 
@@ -180,7 +175,9 @@ CREATE TABLE `tips` (
 
 INSERT INTO `tips` (`id_tips`, `nama_pakar`, `profesi_pakar`, `isi_tips`, `id_step_tips`) VALUES
 (1, 'Jessi Lane Adams', 'Barista', 'Coffee smells like freshly ground heaven.', 3),
-(2, 'Debasish Mridha', 'Philosopher', 'Greatness is not a flower that grows in an easy life, it grows in the adversity of life.', 9);
+(2, 'Debasish Mridha', 'Philosopher', 'Greatness is not a flower that grows in an easy life, it grows in the adversity of life.', 9),
+(3, 'Sir Ranulph Fiennes', 'English Explorer', 'There is no such thing as bad weather, only inappropriate clothing', 10),
+(4, 'Rachel Zoe', 'American Fashion Designer', 'Style is a way to say who you are, without having to speak.', 11);
 
 --
 -- Indexes for dumped tables
@@ -213,12 +210,6 @@ ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indexes for table `pesan`
---
-ALTER TABLE `pesan`
-  ADD PRIMARY KEY (`id_pesan`);
-
---
 -- Indexes for table `step`
 --
 ALTER TABLE `step`
@@ -246,7 +237,7 @@ ALTER TABLE `ads`
 -- AUTO_INCREMENT for table `artikel`
 --
 ALTER TABLE `artikel`
-  MODIFY `id_artikel` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_artikel` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -255,22 +246,16 @@ ALTER TABLE `kategori`
   MODIFY `id_kategori` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `pesan`
---
-ALTER TABLE `pesan`
-  MODIFY `id_pesan` int(10) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `step`
 --
 ALTER TABLE `step`
-  MODIFY `id_step` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_step` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tips`
 --
 ALTER TABLE `tips`
-  MODIFY `id_tips` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_tips` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
